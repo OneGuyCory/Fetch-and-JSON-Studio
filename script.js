ba3735cd8c926b81e7b85c977521a1a8383bc92a
Cory <corydeuschle92@gmail.com>
2022-07-20 16:48:44 -0500

    html test done

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // TODO: add code here
+window.addEventListener('load', function() {
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response) {
+        response.json().then(function(json) {
+            const astronauts = document.getElementById('container');
+            container.innerHTML = `
+            <div class = 'astronaut'>
+                <div class = 'bio'>
+                    <h3>${json[0].firstName} ${json[0].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${json[0].hoursInSpace}</li>
+                        <li>Active: ${json[0].active}</li>
+                        <li>Skills: ${json[0].skills}</li>
+                    </ul>
+                </div>
+                <img class = 'avatar' src = ${json[0].picture}
+            </div>
+            `;
+        });
+        
+    });
+});
