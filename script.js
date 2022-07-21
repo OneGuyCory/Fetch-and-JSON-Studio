f2f7fd754c49675d0c64d8334885e38fa10d97d7
Cory <corydeuschle92@gmail.com>
2022-07-20 19:46:25 -0500

    finished studio besides bonus mission 2

--- script.js
+++ script.js
@@ -2,21 +2,36 @@
 window.addEventListener('load', function() {
     fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response) {
         response.json().then(function(json) {
-            const astronauts = document.getElementById('container');
-            container.innerHTML = `
-            <div class = 'astronaut'>
-                <div class = 'bio'>
-                    <h3>${json[0].firstName} ${json[0].lastName}</h3>
-                    <ul>
-                        <li>Hours in space: ${json[0].hoursInSpace}</li>
-                        <li>Active: ${json[0].active}</li>
-                        <li>Skills: ${json[0].skills}</li>
-                    </ul>
-                </div>
-                <img class = 'avatar' src = ${json[0].picture}
-            </div>
-            `;
-        });
-        
+            const container = document.getElementById('container');
+            let astronautCount = 0;
+           
+
+            //sorts astronauts by hours in space
+            //json.sort((a, b) => a.hoursInSpace - b.hoursInSpace);
+
+            for(i = 0; i < json.length; i++) {
+                
+                container.innerHTML += `
+                    <div class = 'astronaut'>
+                        <div class = 'bio'>
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li id = 'active'>Active: ${json[i].active}</li>
+                                <li>Skills: ${json[i].skills.join(', ')}</li>
+                            </ul>
+                        </div>
+                        <img class = 'avatar' src = ${json[i].picture}
+                    </div>
+                    `;
+                    if(json[i].active === true) {
+                        document.getElementById('active').style.color = 'green';
+                    }
+
+                    astronautCount += 1;
+            }
+            
+            console.log(astronautCount);
+        });   
     });
 });
